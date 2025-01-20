8302d253eddedfeaacc3f55601eeeb6f8b825355
M. Ibrahim Israr <israr.m.ibrahim@gmail.com>
2025-01-20 11:26:48 -0800

    Create weapons array and add weapon object with name and power attributes

--- script.js
+++ script.js
@@ -16,6 +16,24 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const weapons = [
+  {
+    name: "stick",
+    power: 5
+  },
+  {
+    name: "dagger",
+    power: 30
+  },
+  {
+    name: "claw hammer",
+    power: 50
+  },
+  {
+    name: "sword",
+    power: 100
+  }
+];
 const locations = [
   {
     name: "town square",
@@ -75,7 +93,7 @@ function buyHealth() {
     goldText.innerText = gold;
     healthText.innerText = health;
   } else {
-    text.innerText = "You do not have enough gold to buy health."
+    text.innerText = "You do not have enough gold to buy health.";
   }
 }
 
