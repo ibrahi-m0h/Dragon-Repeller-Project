23f6d2c51f72cef793bd4ba38bde55cb8f2b4664
M. Ibrahim Israr <israr.m.ibrahim@gmail.com>
2025-01-27 22:06:23 -0800

    Using Math random multiplied with player's xp, damage is done to monster with every attack

--- script.js
+++ script.js
@@ -162,10 +162,12 @@ function goFight() {
 function attack() {
   text.innerText = "The " + monsters[fighting].name + " attacks.";
   text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + ".";
-
+  health -= monsters[fighting].level; // Monster decreases your health
+  monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+  healthText.innerText = health
+  monsterHealthText.innerText = monsterHealth
 }
 
 function dodge() {
 
 }
-
